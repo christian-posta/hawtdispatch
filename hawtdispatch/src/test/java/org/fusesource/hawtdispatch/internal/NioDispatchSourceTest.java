2590eb5afa08759de125479ddf8a4e56caef407d
Christian Posta <christian.posta@gmail.com>
2012-06-28 11:22:42 -0700

    added some code/test to learn a little more about the NIO selector implementation in HawtDispatch

--- hawtdispatch/src/test/java/org/fusesource/hawtdispatch/internal/NioDispatchSourceTest.java
+++ hawtdispatch/src/test/java/org/fusesource/hawtdispatch/internal/NioDispatchSourceTest.java
@@ -1,12 +1,18 @@
 package org.fusesource.hawtdispatch.internal;
 
 import com.sun.org.apache.bcel.internal.generic.NEW;
+import org.fusesource.hawtdispatch.DispatchPriority;
 import org.fusesource.hawtdispatch.DispatchQueue;
+import org.fusesource.hawtdispatch.Task;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.nio.channels.SelectableChannel;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.*;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 
@@ -18,6 +24,7 @@ public class NioDispatchSourceTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testCannotInstantiateZeroSelectionOp() {
+
         DispatcherConfig config = mock(DispatcherConfig.class);
         HawtDispatcher dispatcher = new HawtDispatcher(config);
         SelectableChannel channel = mock(SelectableChannel.class);
@@ -29,19 +36,82 @@ public class NioDispatchSourceTest {
 
     }
 
-    @Test
-    public void testPickThreadQueue() {
-        fail("write test for NioDispatchSource#pickThreadQueue()");
+    @Test(expected = NullPointerException.class)
+    public void testNullTargetQueue() {
+
+        HawtDispatcher dispatcher = new DispatcherConfig().createDispatcher();
+        SelectableChannel channel = mock(SelectableChannel.class);
+        int interestOps = 1;
+
+        // target channel cannot be null
+        new NioDispatchSource(dispatcher, channel, interestOps, null);
+
     }
 
     @Test
-    public void foo() {
+    public void testCreation() {
         HawtDispatcher dispatcher = new DispatcherConfig().createDispatcher();
         SelectableChannel channel = mock(SelectableChannel.class);
         DispatchQueue queue = dispatcher.createQueue("test");
-
-        // cannot be zero
         int interestOps = 1;
-        new NioDispatchSource(dispatcher, channel, interestOps, queue);
+
+        NioDispatchSource source = new NioDispatchSource(dispatcher, channel, interestOps, queue);
+        assertEquals(queue, source.getTargetQueue());
+        assertTrue(source.isSuspended());
+        assertEquals(null, source.getData());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testResumeNoEventHandler() throws IOException {
+        HawtDispatcher dispatcher = new DispatcherConfig().createDispatcher();
+        SelectableChannel channel = mock(SelectableChannel.class);
+        DispatchQueue queue = dispatcher.createQueue("test");
+        int interestOps = 1;
+
+        NioDispatchSource source = new NioDispatchSource(dispatcher, channel, interestOps, queue);
+
+        // should fail, there is no event handler!!
+        source.resume();
+
+    }
+
+    private SelectableChannel createSocketChannel(int port) throws IOException {
+        ServerSocketChannel channel = ServerSocketChannel.open();
+        channel.configureBlocking(false);
+        channel.socket().bind(new InetSocketAddress(port));
+        return channel;
+    }
+
+    @Test
+    public void testWorkerThreadHasSelectorKeyOnResume() throws IOException, InterruptedException {
+        HawtDispatcher dispatcher = new DispatcherConfig().createDispatcher();
+        SelectableChannel channel = createSocketChannel(4444);
+        DispatchQueue queue = dispatcher.createQueue("test");
+
+        NioDispatchSource source = new NioDispatchSource(dispatcher, channel, SelectionKey.OP_ACCEPT, queue);
+        source.setEventHandler(new Task(){
+
+            @Override
+            public void run() {
+                System.out.println("hello, from the event source");
+            }
+        });
+
+        source.resume();
+
+        // wait a sec for the channel to become registered on resume()...
+        Thread.sleep(1000);
+
+        boolean oneThreadHasSelectorForThisChannel = false;
+        WorkerThread[] threads = dispatcher.DEFAULT_QUEUE.workers.getThreads();
+        for (WorkerThread workerThread : threads) {
+            Selector selector = workerThread.getNioManager().getSelector();
+            SelectionKey key = channel.keyFor(selector);
+            if (key != null) {
+                oneThreadHasSelectorForThisChannel = true;
+            }
+        }
+
+        assertTrue(oneThreadHasSelectorForThisChannel);
     }
 }
---
+++ hawtdispatch/src/test/java/org/fusesource/hawtdispatch/internal/SelectorNonBlockingSocketTest.java
@@ -0,0 +1,165 @@
+package org.fusesource.hawtdispatch.internal;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.AbstractSelectableChannel;
+import java.util.Iterator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cposta
+ * Date: 6/28/12
+ * Time: 8:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SelectorNonBlockingSocketTest {
+
+    public static void main(String[] args) {
+
+        Selector selector = null;
+
+        try {
+            selector = Selector.open();
+            ServerSocketChannel channel1 = createSocketChannel(4444);
+            ServerSocketChannel channel2 = createSocketChannel(4445);
+
+            // register all selection options on the channel to the selector
+            channel1.register(selector, SelectionKey.OP_ACCEPT);
+            channel2.register(selector, SelectionKey.OP_ACCEPT);
+
+        } catch (IOException e) {
+            System.out.println("Errors creating the selector");
+        }
+
+        System.out.println("Waiting for an event...");
+
+        while (true) {
+            try {
+                selector.select();
+
+            } catch (IOException e) {
+                break;
+            }
+
+            Iterator it = selector.selectedKeys().iterator();
+            while (it.hasNext()) {
+                SelectionKey selectionKey = (SelectionKey) it.next();
+
+                // remote it from the list to indicate it's being processed
+                it.remove();
+
+                if (selectionKey.isAcceptable()) {
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
+                    int localPort = serverSocketChannel.socket().getLocalPort();
+                    try {
+                        SocketChannel socketChannel = serverSocketChannel.accept();
+                        processSocketChannel(socketChannel);
+                    } catch (IOException e) {
+                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    }
+
+                }
+            }
+        }
+    }
+
+    private static void processSocketChannel(SocketChannel socketChannel) {
+        if (socketChannel == null) {
+            // ignore because there are no events
+        }
+        else {
+            try {
+                ByteBuffer buf = ByteBuffer.allocate(1024);
+                int numBytes = socketChannel.read(buf);
+                if (numBytes == -1) {
+                    socketChannel.close();
+                } else {
+                    buf.flip();
+                    System.out.println("this is what we go: " + new String(buf.array()));
+                    socketChannel.write(buf);
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+    }
+
+    private static ServerSocketChannel createSocketChannel(int port) throws IOException {
+        ServerSocketChannel channel = ServerSocketChannel.open();
+        channel.configureBlocking(false);
+        channel.socket().bind(new InetSocketAddress(port));
+        System.out.println("Opened a socket");
+        return channel;
+    }
+
+
+    public static void processSelection(SelectionKey selectionKey) throws IOException {
+
+        if (selectionKey.isValid() && selectionKey.isConnectable()) {
+            // get the channel with the connection request
+            SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+
+            boolean success = socketChannel.finishConnect();
+            if (!success) {
+                // An error occurred, handle it
+                System.out.println("Error happened!!");
+
+                selectionKey.cancel();
+            }
+
+        }
+
+        if (selectionKey.isValid() && selectionKey.isReadable()) {
+            // get channel with bytes to read
+//            SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+//
+//            ByteBuffer buf = ByteBuffer.allocate(1024);
+//            try {
+//                buf.clear();
+//                int numBytesRead = socketChannel.read(buf);
+//                if (numBytesRead == -1) {
+//                    // no more bytes to read
+//                    socketChannel.close();
+//                } else {
+//                    buf.flip();
+//                    System.out.println("What we got: ");
+//                    System.out.write(buf.array());
+//                }
+//
+//            } catch (IOException e) {
+//                // conn could be closed?
+//                System.out.println("Connection could be closed");
+//            }
+            System.out.println("Read event...");
+
+        }
+
+        if (selectionKey.isValid() && selectionKey.isWritable()) {
+            // get channel with bytes to write
+//            SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+//            ByteBuffer buf = ByteBuffer.allocate(1024);
+//            try {
+//                buf.put("you made it!".getBytes());
+//                buf.flip();
+//                int numBytesWritten = socketChannel.write(buf);
+//                System.out.println("Number of bytes written: " + numBytesWritten);
+//
+//            } catch (IOException e) {
+//                System.out.println("Connection could be closed");
+//            }
+
+            System.out.println("write event");
+
+
+        }
+    }
+}
