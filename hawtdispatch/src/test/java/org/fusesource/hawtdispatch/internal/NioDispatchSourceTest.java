ae45a8de252abaa5237b22a9e5a44b7369072bf2
Christian Posta <christian.posta@gmail.com>
2012-06-28 15:52:57 -0700

    added some code/test to learn a little more about the NIO selector implementation in HawtDispatch

--- hawtdispatch/src/test/java/org/fusesource/hawtdispatch/internal/NioDispatchSourceTest.java
+++ hawtdispatch/src/test/java/org/fusesource/hawtdispatch/internal/NioDispatchSourceTest.java
@@ -4,12 +4,17 @@ import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.fusesource.hawtdispatch.DispatchPriority;
 import org.fusesource.hawtdispatch.DispatchQueue;
 import org.fusesource.hawtdispatch.Task;
+import org.fusesource.hawtdispatch.internal.util.RunnableCountDownLatch;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.nio.channels.*;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -75,43 +80,111 @@ public class NioDispatchSourceTest {
 
     }
 
-    private SelectableChannel createSocketChannel(int port) throws IOException {
+    private ServerSocketChannel createSocketChannel(int port) throws IOException {
         ServerSocketChannel channel = ServerSocketChannel.open();
         channel.configureBlocking(false);
-        channel.socket().bind(new InetSocketAddress(port));
+        channel.socket().bind(address("0.0.0.0", port));
         return channel;
     }
 
     @Test
     public void testWorkerThreadHasSelectorKeyOnResume() throws IOException, InterruptedException {
         HawtDispatcher dispatcher = new DispatcherConfig().createDispatcher();
-        SelectableChannel channel = createSocketChannel(4444);
+
+        ServerSocketChannel channel = createSocketChannel(4444);
         DispatchQueue queue = dispatcher.createQueue("test");
 
-        NioDispatchSource source = new NioDispatchSource(dispatcher, channel, SelectionKey.OP_ACCEPT, queue);
-        source.setEventHandler(new Task(){
+        try {
+            NioDispatchSource source = new NioDispatchSource(dispatcher, channel, SelectionKey.OP_ACCEPT, queue);
+            source.setEventHandler(new Task(){
 
-            @Override
-            public void run() {
-                System.out.println("hello, from the event source");
+                @Override
+                public void run() {
+                    System.out.println("hello, from the event source");
+                }
+            });
+
+            source.resume();
+
+            // wait a sec for the channel to become registered on resume()...
+            Thread.sleep(1000);
+
+            boolean oneThreadHasSelectorForThisChannel = false;
+            WorkerThread[] threads = dispatcher.DEFAULT_QUEUE.workers.getThreads();
+            for (WorkerThread workerThread : threads) {
+                Selector selector = workerThread.getNioManager().getSelector();
+                SelectionKey key = channel.keyFor(selector);
+                if (key != null) {
+                    oneThreadHasSelectorForThisChannel = true;
+                }
             }
-        });
 
-        source.resume();
+            assertTrue(oneThreadHasSelectorForThisChannel);
+        }
+        finally {
+            channel.close();
+        }
+    }
 
-        // wait a sec for the channel to become registered on resume()...
-        Thread.sleep(1000);
+    @Test
+    public void testFire() throws IOException, InterruptedException {
 
-        boolean oneThreadHasSelectorForThisChannel = false;
-        WorkerThread[] threads = dispatcher.DEFAULT_QUEUE.workers.getThreads();
-        for (WorkerThread workerThread : threads) {
-            Selector selector = workerThread.getNioManager().getSelector();
-            SelectionKey key = channel.keyFor(selector);
-            if (key != null) {
-                oneThreadHasSelectorForThisChannel = true;
-            }
+
+        HawtDispatcher dispatcher = new DispatcherConfig().createDispatcher();
+
+        ServerSocketChannel channel = createSocketChannel(4444);
+        DispatchQueue queue = dispatcher.createQueue("test");
+        RunnableCountDownLatch ran = acceptor(channel);
+
+        try {
+            final NioDispatchSource source = new NioDispatchSource(dispatcher, channel, SelectionKey.OP_ACCEPT, queue);
+            source.setEventHandler(ran);
+
+            source.resume();
+
+            Thread.sleep(1000);
+
+            // wait a sec for the channel to become registered on resume()...
+            dispatcher.DEFAULT_QUEUE.workers.getThreads()[0].getDispatchQueue().execute(new Task() {
+                @Override
+                public void run() {
+                    source.fire(SelectionKey.OP_ACCEPT);
+
+                }
+            });
+
+        }finally {
+            channel.close();
         }
 
-        assertTrue(oneThreadHasSelectorForThisChannel);
+        assertTrue(ran.await(1, TimeUnit.SECONDS));
     }
+
+
+    static public InetSocketAddress address(String host, int port) throws UnknownHostException {
+        return new InetSocketAddress(ip(host), port);
+    }
+
+    static public InetAddress ip(String host) throws UnknownHostException {
+        return InetAddress.getByName(host);
+    }
+
+    private RunnableCountDownLatch acceptor(final ServerSocketChannel channel) {
+        return new RunnableCountDownLatch(1) {
+            @Override
+            public void run() {
+                try {
+                    SocketChannel socket = channel.accept();
+                    socket.close();
+                    System.out.println("Ran acceptor");
+                } catch (IOException e) {
+                    // it will fail because of a closed socket because there isn't really
+                    // a selection event... ignore it..
+                }
+                super.run();
+            }
+        };
+    }
+
+
 }
